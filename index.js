50f464b06c4689a9305442a8703d7d73a1370aa8
Gautham <gauthamkumar.gk.gk@gmail.com>
2022-01-03 22:59:35 +0400

    dynamic port

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get("/:code", cacheMiddleware(180), async (req, res) => {
 });
 
 // PORT
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
